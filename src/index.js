bb0cded55a57357cceaf009276fdb0522d78bd50
Hayk <hayk.pilos@gmail.com>
2018-05-07 00:16:47 +0400

    feat(random): Add ability to get an array of footballer names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const uniqueRandomArray = require('unique-random-array');
 const footballerNames = require('./footballer-names.json');
+const getRandomItem = uniqueRandomArray(footballerNames);
 
 module.exports = {
   all: footballerNames,
-  random: uniqueRandomArray(footballerNames),
+  random: random,
 };
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  }
+
+  const randomItems = [];
+  for (let i = 0; i < number; i++) {
+    randomItems.push(getRandomItem());
+  }
+  return randomItems;
+}
--- src/index.test.js
+++ src/index.test.js
@@ -12,5 +12,20 @@ describe('footballer-names', function () {
         });
       }
     });
+  });
+
+  describe('random', function() {
+    it('should return a random item from', function () {
+      const randomItem = footballers.random();
+      expect(footballers.all).to.include(randomItem);
+    });
+
+    it('should return an array of random items if passed a number', function () {
+      const randomItems = footballers.random(3);
+      expect(randomItems).to.have.length(3);
+      randomItems.forEach(function(item) {
+        expect(footballers.all).to.include(item);
+      });
+    });
   })
 });
